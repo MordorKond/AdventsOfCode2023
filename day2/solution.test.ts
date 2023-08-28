37525ec21537bb950d8919e3978cd7d53c868d26
Ivaylo Nakov <i.nakov18@gmail.com>
2023-08-28 23:59:29 +0200

    First attempt

--- day2/solution.test.ts
+++ day2/solution.test.ts
@@ -2,8 +2,8 @@ import { main } from "./solution";
 
 
 describe('sum module', () => {
-  test('sum the numbers', () => {
-    expect(main(1,3)).toBe(4);
+  test('sum the numbers', async () => {
+    expect(await main()).toBe(4);
   });
 });
 
--- day2/solution.ts
+++ day2/solution.ts
@@ -1,3 +1,27 @@
-export function main(a:number,b:number){
-    return a+b;
+import { open } from 'node:fs/promises';
+
+export async  function main(){
+    const file = await open('./input.txt');
+    if (!file)return 'no file to read';
+
+    //RPS 1 2 3
+    //LDW 0 3 6
+    //RPS A B C
+    //RPS X Y Z
+
+    const RPS = ['A','B','C','A']
+    const RPS2 = ['X','Y','Z','X']
+    
+    for await (const line of file.readLines()) {
+        const choises = line.split(' ');
+        const elf = choises[0]
+        const opo = choises[1]
+        let temp = 0;
+        temp+=RPS.indexOf(elf)+1;
+        if(RPS.indexOf(elf)<RPS2.indexOf(opo))temp+=0;
+        if(RPS.indexOf(elf)==RPS2.indexOf(opo))temp+=3;
+        if(RPS.indexOf(elf)>RPS2.indexOf(opo))temp+=6;
+    
+        return { line, temp };
+    }
 };
