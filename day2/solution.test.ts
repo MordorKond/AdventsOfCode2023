41ea46d728bca2ca1d7e2899ed21560f79db67a6
Ivaylo Nakov <i.nakov18@gmail.com>
2023-08-29 23:34:04 +0200

    Part 1 solved.

--- day2/solution.test.ts
+++ day2/solution.test.ts
@@ -1,9 +1,43 @@
 import { main } from "./solution";
 
+    //RPS 1 2 3
+    //LDW 0 3 6
+    //RPS A B C
+    //RPS X Y Z
+
+/* describe('sum module', () => {
+  test('R R', () => {
+    expect(main(['A X'])).toBe(1+3);
+  }),
+  test('R P', () => {
+    expect(main(['A Y'])).toBe(2+6);
+  }),
+  test('R S', () => {
+    expect(main(['A Z'])).toBe(3+0);
+  }),
+  test('P R', () => {
+    expect(main(['B X'])).toBe(1+0);
+  }),
+  test('P P', () => {
+    expect(main(['B Y'])).toBe(2+3);
+  }),
+  test('P S', () => {
+    expect(main(['B Z'])).toBe(3+6);
+  }),
+  test('S R', () => {
+    expect(main(['C X'])).toBe(1+6);
+  }),
+  test('S P', () => {
+    expect(main(['C Y'])).toBe(2+0);
+  }),
+  test('S S', () => {
+    expect(main(['C Z'])).toBe(3+3);
+  });
+}); */
 
 describe('sum module', () => {
-  test('sum the numbers', async () => {
-    expect(await main()).toBe(4);
+  test('run full list', async () => {
+    expect(await main()).toBe(1+3);
   });
 });
 
--- day2/solution.ts
+++ day2/solution.ts
@@ -1,27 +1,49 @@
 import { open } from 'node:fs/promises';
 
-export async  function main(){
+// export function main(arr:string[]){
+export async function main(){
     const file = await open('./input.txt');
     if (!file)return 'no file to read';
 
+    //INX 0 1 2
     //RPS 1 2 3
     //LDW 0 3 6
     //RPS A B C
     //RPS X Y Z
 
-    const RPS = ['A','B','C','A']
-    const RPS2 = ['X','Y','Z','X']
+    const RPS = ['X','Y','Z','X']
+    const RPS2 = ['A','B','C','A']
     
+    let score = 0;
+    let tempTest = [];
     for await (const line of file.readLines()) {
+    // for (const line of arr) {
         const choises = line.split(' ');
-        const elf = choises[0]
-        const opo = choises[1]
-        let temp = 0;
-        temp+=RPS.indexOf(elf)+1;
-        if(RPS.indexOf(elf)<RPS2.indexOf(opo))temp+=0;
-        if(RPS.indexOf(elf)==RPS2.indexOf(opo))temp+=3;
-        if(RPS.indexOf(elf)>RPS2.indexOf(opo))temp+=6;
-    
-        return { line, temp };
+        const elf = RPS.indexOf(choises[1]);
+        const opo = RPS2.indexOf(choises[0]);
+        let temp = elf+1
+        score+=temp;
+        tempTest.push(temp);
+        const result = elf-opo;
+        const resultAbs = Math.abs(result)
+
+        if(resultAbs==0){
+            temp=3
+            score+=temp
+            tempTest.push(temp)
+        }
+        else if(resultAbs==1){
+            temp=result>0?6:0
+            score+=temp
+            tempTest.push(temp)
+        }
+        else if(resultAbs==2){
+            temp=result>0?0:6
+            score+=temp
+            tempTest.push(temp)
+        }
+
     }
+    return score;
+    // return { temp: score, tempTest };
 };
